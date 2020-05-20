cf539513fc9417dae8da11c2722e3f486659bff9
Duc Thang Vu <vuducthang91@gmail.com>
2020-05-20 14:54:44 +0200

    php json created

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-# TITLE
+# PHP-AJAX-DISCHI 
 
-## Feature
-
-
-## Usage
-
-
-
-## Contributing
-
-
-## License 
+PHP Dischi Milestone 1
+Descrizione
+Stampare a schermo una decina di dischi musicali (vedi grafica allegata sotto)
+Utilizzare solo PHP per ora e non AJAX.
+Stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+Utilizzare Html, Sass e PHP
+Scegliete pure le immagini che volete utilizzare
--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php-ajax-dischi</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="dist/css/main.css">
 </head>
 <body>
-    
+    <header>
+        
+    </header>
 
 
     <!-- TEMPLATE-->
