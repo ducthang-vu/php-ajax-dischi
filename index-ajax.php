107ced56e6e8968f5ffa641aa5abb95179a8a78f
Duc Thang Vu <vuducthang91@gmail.com>
2020-05-21 12:52:02 +0200

    removed unused scripts

--- index-ajax.php
+++ index-ajax.php
@@ -30,8 +30,7 @@
 
     <!-- SCRIPT -->
     <!--<script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js'></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.6/handlebars.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>-->
+    -->
     <script src="https://kit.fontawesome.com/89ef06fff6.js" crossorigin="anonymous"></script>
     <script src="dist/script/main.js"></script>
 </body>
