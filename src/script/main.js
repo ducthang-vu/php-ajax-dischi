0fc9560df0c161bd8e6319264244b62060b7664b
Duc Thang Vu <vuducthang91@gmail.com>
2020-05-21 11:44:07 +0200

    handlebars installed

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,9 @@
 import $ from 'jquery'
+const Handlebars = require("handlebars")
 
 console.log('main.js is working')
 console.log($)
+console.log(Handlebars)
 
 $(document).ready(function () {
     
